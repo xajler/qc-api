49452506ab8b82cbb659b2b0627227ba1bfdc56d
Kornelije Sajler <xajler@gmail.com>
2012-08-19 01:18:52 +0200

    Initial commit. Added all needed gems for testing/development/db

---
+++ Gemfile
@@ -0,0 +1,28 @@
+source 'https://rubygems.org'
+
+gem 'rails', '~> 3.2.8'
+gem 'rails-api'
+gem 'pg'
+gem 'rabl'
+
+group :development do
+  gem 'sqlite3'
+  gem 'capistrano'
+end
+
+group :test, :development do
+  gem 'rspec-rails'
+  gem 'pry'
+end
+
+group :test do
+  gem 'factory_girl_rails'
+  gem 'rb-fsevent'
+  gem 'guard-rspec'
+  gem 'guard-spork'
+  gem 'fuubar'
+end
+
+
+# gem 'bcrypt-ruby', '~> 3.0.0'
+# gem 'unicorn'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+QcoachApi::Application.config.secret_token = '4aa3dad9973d96d60926faef120d2e06ba490189d35f1a634cd4b6028b3596581e307c99ba3a099fe5eac09e0ba43329f0fae373376b342f49d86fa43c396894'
